1b4b106c73066ddac5aa7e039172657729b40382
Stephen Contreras <stephen@stephendoes.dev>
2020-10-27 10:47:15 -0500

    maybe mobile bg fix

---
+++ src/Back.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './components/mysvg.css'
+import './Back.css'
+class Back extends React.Component {
+  render(){
+      return (
+      <div id='dots' className="bgsvg">
+       test
+      </div>
+    );
+    }
+}
+
+export default Back;
